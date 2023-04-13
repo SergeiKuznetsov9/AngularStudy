10c01dcc2f2bcb4b261cf8fd574fab900a67e5a8
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-04-13 16:20:17 +0300

    ADD: grid basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './components/main/main.component';
+import { BasicComponent } from './components/basic/basic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    BasicComponent
   ],
   imports: [
     BrowserModule,
